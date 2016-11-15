f8c9b1a91f7d8c68f6228baf47df03ad31cdea81
Navi Kaur <nkaur901@gmail.com>
2016-11-15 18:18:47 -0500

    created a child for each animal, made the rhino roar and bluewhale swim

--- src/data.js
+++ src/data.js
@@ -19,9 +19,19 @@
   BlueWhale.prototype = Object.create(Animal.prototype);
   BlueWhale.prototype.constructor = BlueWhale;
 
+  BlueWhale.prototype.giveBirth = function giveBirth() {
+    var baby = new BlueWhale('baby whale', 2016);
+    return baby;
+  }
+
+  BlueWhale.prototype.swim = function swim(duration) {
+    return ("I'm swimming for an hour!");
+  }
 
   var bw = new BlueWhale('matt', 2000);
   console.log(bw);
+  console.log(bw.giveBirth());
+  console.log(bw.swim());
 
   function BlackRhino(name, dob) {
     Animal.apply(this, [name, dob]);
@@ -30,8 +40,19 @@
   BlackRhino.prototype = Object.create(Animal.prototype);
   BlackRhino.prototype.constructor = BlackRhino;
 
+  BlackRhino.prototype.giveBirth = function giveBirth() {
+    var baby = new BlackRhino('baby rhino', 2015);
+    return baby;
+  }
+
+  BlackRhino.prototype.roar = function roar(arg) {
+    return 'ROAR!!';
+  }
+
   var br = new BlackRhino('matteo', 2005);
   console.log(br);
+  console.log(br.giveBirth());
+  console.log(br.roar("hi!"));
 
 
 
