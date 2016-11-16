19d422cdeb9c762b46b9ba45b55262a139911a98
Navi Kaur <nkaur901@gmail.com>
2016-11-16 16:55:20 -0500

    created a type error if no name or date of birth are given

--- src/data.js
+++ src/data.js
@@ -1,27 +1,65 @@
 (function() {
   'use strict';
 
+  window.zoo = window.zoo || {};
+
+  window.zoo.Animal = Animal;
+  window.zoo.BlueWhale = BlueWhale;
+  window.zoo.BlackRhino = BlackRhino;
+
+/**
+ * This will create a new animal when given a new name and date of birth
+ * @param {String} name The name of the new animal
+ * @throws {TypeError} If the arguments are not a string and a Number
+ * @param {Number} dob  the number for its date of birth
+ */
   function Animal(name, dob) {
-    this.name = name || 'no name';
+    if (typeof(name) !== 'string') {
+      var err = new TypeError ('Hey! The name has to be a string!');
+      err.userMessage = 'Sorry, our developers are cray cray';
+      throw err;
+    }
+
+    if (typeof(dob) !== 'number') {
+      var err = new TypeError ('Hey! The date of birth has to be a number!');
+      err.userMessage = 'Sorry, our developers did not get this right...'
+      throw err;
+    }
+
+    this.name = name || 'no name'; // "this" points to the "Animal" we're creating at that time
     this.dob = dob;
   }
-  
-  var bill = new Animal('bill', 2009);
-  var jack = new Animal('jack', 2006);
 
-  console.log('b:', bill);
-  console.log('j:', jack);
+  // var bill = new Animal('bill', 2009);
+  // var jack = new Animal('jack', 2006);
+  //
+  // console.log('b:', bill);
+  // console.log('j:', jack);
 
+  /**
+   * Giving the BlueWhale a name and dob
+   * @param {String} name This will give a new name
+   * @param {Number} dob  The dob of the BlueWhale
+   */
   function BlueWhale(name, dob) {
-    Animal.apply(this, [name, dob]);
+    Animal.apply(this, [name, dob]); // .apply tells Animal to execute but once inside
+                                    // I want this to point to new Narwhal
+                                    // Each fn has its own this
+                                    // apply says change my context to whatever I put inside (this
+                                    // inside .apply(points to Narwhal)
   }
 
-  BlueWhale.prototype = Object.create(Animal.prototype);
-  BlueWhale.prototype.constructor = BlueWhale;
+  BlueWhale.prototype = Object.create(Animal.prototype);// this sets up the chain
+  //here we say, what it means to be a BlueWhale is what it means to be an Animal
+  //base the BlueWhale prototype on the animal prototype
+  //above, we make bluewhale prototype equal to animal protype but we want to flip it back to bluewhale
+  BlueWhale.prototype.constructor = BlueWhale; // this flips the constructor back to Bluewhale
+  BlueWhale.prototype.breed = 'BlueWhale';
 
-  BlueWhale.prototype.giveBirth = function giveBirth() {
-    var baby = new BlueWhale('baby whale', 2016);
-    return baby;
+  BlueWhale.prototype.giveBirth = function giveBirth() { // Instance Method:
+    var baby = new BlueWhale('baby whale', 2016);        //function that is on an object like giveBirth
+                                                        //
+    return baby; // OR return new Bluewhale('baby whale', 2016)
   }
 
   BlueWhale.prototype.swim = function swim(duration) {
@@ -33,12 +71,18 @@
   console.log(bw.giveBirth());
   console.log(bw.swim());
 
+  /**
+   * This creates a BlackRhino that takes a name and an age because it's an animal
+   * @param {String} name The name of BlackRhino
+   * @param {Number} dob  The date of birth of BlackRhino
+   */
   function BlackRhino(name, dob) {
-    Animal.apply(this, [name, dob]);
+    Animal.apply(this, [name, dob]); // .apply lets us change the context and it's arguments
   }
 
   BlackRhino.prototype = Object.create(Animal.prototype);
   BlackRhino.prototype.constructor = BlackRhino;
+  BlackRhino.prototype.breed = 'BlackRhino';
 
   BlackRhino.prototype.giveBirth = function giveBirth() {
     var baby = new BlackRhino('baby rhino', 2015);
--- test/specs/zoo.spec.js
+++ test/specs/zoo.spec.js
@@ -5,18 +5,71 @@
   var expect = chai.expect;
 
   describe ('zoo objectives', function () {
-    it('should know that one equals one', function() {
-      expect(1).to.equal(1);
+    it('should know that the namespace exists', function() {
+      expect(window.zoo).to.be.an('object');
+      expect(window.zoo.Animal).to.be.an('function');
+      expect(window.zoo.BlueWhale).to.be.an('function');
+      expect(window.zoo.BlackRhino).to.be.an('function');
+
     });
 
-  describe('constructor function', function () {
-    it('should know that Animal is a construct function', function () {
-      expect()
-    })
-  }
+  describe('zoo animal', function () {
 
+      it('should create new animal', function() {
+        var jack = new window.zoo.Animal('jack', 2013);
+        expect(jack).to.be.an.instanceof(window.zoo.Animal);
+      });
 
+      it('should have a date of birth', function() {
+        var jack = new window.zoo.Animal('jack', 2013);
+        expect(jack.dob).to.equal(2013);
+      });
 
+      it('should have a name', function () {
+        var jack = new window.zoo.Animal('jack', 2013);
+        expect(jack.name).to.equal('jack');
+      });
+
+      it('should produce an error if no name is given', function() {
+        try {
+          new window.zoo.Animal();
+          expect(true).to.be.false;
+        } catch (err) {
+            expect(err).to.be.an.instanceof(Object);
+            expect(err).to.be.an.instanceof(TypeError);
+        }
+      });
+
+      it('should produce an error if no date of birth is given', function () {
+        try {
+          new window.zoo.Animal('matt');
+          expect(true).to.be.false;
+        } catch (err) {
+            console.log( err );
+            expect(err).to.be.an.instanceof(Object);
+            expect(err).to.be.an.instanceof(TypeError);
+        }
+      });
+  });
+  // don't need to test if it should produce an error when no name or dob are given
+// test that I can create animals
+//for give birth test if it returns a baby and it's an instance of bluewhale
+      // 2
+
+    // it('should give birth')
+
+  // describe('constructor function', function () {
+  //   it('should know that Animal is a construct function', function () {
+  //     expect()
+  //   })
+  // }
+  //create namespace
+
+// var x = new Animal
+// use instanceof to test if var x is an instanceof an Animal
+// test that each animal has a name & age and its the name and age that I passed in
+
+//3 tests --> giveBirth method
 
 
   });
